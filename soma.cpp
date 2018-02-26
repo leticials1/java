7eb10436b376c4e983afdf88f9bd0f2c05fe5224
leticials1 <36648491+leticials1@users.noreply.github.com>
2018-02-25 22:23:40 -0300

    Add files via upload

---
+++ soma.cpp
@@ -0,0 +1,11 @@
+      #include<stdio.h>
+        #include<stdlib.h>  
+     int soma(int a, int b)
+      { printf("Insira o primeiro valor:");
+        scanf("%d", &a);
+        printf("Insira o segundo valor:");
+        scanf("%d", &b);
+      
+      return a+b;
+      
+      }
