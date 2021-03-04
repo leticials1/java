3e6b023fa8b13482faf3a015e488207bd26d14bc
leticials1 <36648491+leticials1@users.noreply.github.com>
2021-03-04 17:23:11 -0300

    Add files via upload

---
+++ Tabuada.java
@@ -0,0 +1,24 @@
+/* Leticia Siqueira - 0050012430 */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        int entrada;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o numero: ");
+        entrada = scanner.nextInt();
+
+        int multiplicador = 0;
+        while(multiplicador <= 10)
+        {
+            int resultado = entrada * multiplicador;
+            System.out.println(resultado);   
+            multiplicador++;
+        }
+    }
+}
